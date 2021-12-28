2ea7d13235b34b43875a10088062a16e2d7402e6
hniss <nissoul.hamza@gmail.com>
2021-12-28 22:58:51 +0100

    Add controller API

--- src/main/java/fr/emsi/pfa/ui/controllers/ProjetController.java
+++ src/main/java/fr/emsi/pfa/ui/controllers/ProjetController.java
@@ -1,10 +1,49 @@
 package fr.emsi.pfa.ui.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import fr.emsi.pfa.entities.Projet;
+import fr.emsi.pfa.service.implInterfaces.ProjetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/Api/gestionProjet")
 public class ProjetController {
+
+    @Autowired
+    private ProjetService ps;
+
+    @PostMapping("/")
+    public void save(Projet pj){
+        ps.save(pj);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody Projet pj) {
+        this.ps.update(pj);
+    }
+
+    @GetMapping("/id/{id}")
+    public Projet findById(int id) {
+        return ps.findById(id);
+    }
+
+    @DeleteMapping("/deleteAll/")
+    public void deleteAll() {
+        ps.deleteAll();
+    }
+
+    @GetMapping("/findAll/")
+    public List<Projet> findAll() {
+        return ps.findAll();
+    }
+
+    @DeleteMapping("/deleteId/{id}")
+    public void deleteById(@PathVariable int id) {
+        ps.deleteById(id);
+    }
+
 
 }
